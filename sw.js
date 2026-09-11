1bca65fdbfce12db482f1201993ea1129073aa89
noxpro0 <yakupaslanksk3355@gmail.com>
2026-09-11 16:05:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kupa-kizi-v1';
+const CACHE = 'kupa-kizi-v2';
 const ASSETS = ['./', './index.html', './the-kiss.jpg', './icon-192.png', './icon-512.png', './manifest.json'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
